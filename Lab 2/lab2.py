755cc89f740c3190aefbbfcd6553aa90dfd8239a
bashair yaqoob <bashairyaqoob@gmail.com>
2026-01-28 09:37:25 +0500

    Update lab2.py

--- Lab 2/lab2.py
+++ Lab 2/lab2.py
@@ -1,1 +1,16 @@
+#architecture of class
+class person: 
+    pass #future implementation
+
+s= person()
+#---------------
+class person: 
+    x=10
+    def display(self):
+        print("person")
+
+s= person()
+s.display()
+print(s.x)
+#-------------------
 
